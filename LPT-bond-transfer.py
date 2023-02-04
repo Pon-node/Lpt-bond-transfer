de34ad96ccf00f7b3d0929c7b0f9a86ebc3be249
Pon <94224680+Pon-node@users.noreply.github.com>
2023-02-04 21:56:52 +0000

    Update LPT-bond-transfer.py

--- LPT-bond-transfer.py
+++ LPT-bond-transfer.py
@@ -10,7 +10,7 @@ RECEIVER_PUBLIC_KEY = 'WalletThatWillReveiveBondAddress'
 L2_RPC_PROVIDER = 'https://arb1.arbitrum.io/rpc'
 
 # global constants
-WAIT_TIME_ROUND_LOCK = 300
+WAIT_TIME_ROUND_LOCK = 60
 WAIT_TIME_BALANCE_CHECK = 14400
 BONDING_CONTRACT_ADDR = '0x35Bcf3c30594191d53231E4FF333E8A770453e40'
 ROUNDS_CONTRACT_ADDR = '0xdd6f56DcC28D3F5f27084381fE8Df634985cc39f'
@@ -29,8 +29,8 @@ def getABI(path):
         exit(1)
 
 """
-@brief Returns the checksum of the provided public key
-@param wallet: raw public key as string
+@brief Returns a JSON object of ABI data
+@param path: absolute/relative path to an ABI file
 """
 def getChecksumAddr(wallet):
     try:
@@ -48,13 +48,15 @@ def getChecksumAddr(wallet):
 def waitForStake(bonding_contract, delegator):
     # get delegator info (returns [bondedAmount, fees, delegateAddress, delegatedAmount, startRound, lastClaimRound, nextUnbondingLockId])
     delegator_info = bonding_contract.functions.getDelegator(delegator).call()
-    staked_lpt = web3.Web3.fromWei(delegator_info[0], 'ether')
+    pending_lptu = bonding_contract.functions.pendingStake(delegator, 99999).call()
+    pending_lpt = web3.Web3.fromWei(pending_lptu, 'ether')
+    staked_lpt = web3.Web3.fromWei(pending_lptu, 'ether')
     print("Delegator is currently staking {0:.2f} LPT @ {1}".format(staked_lpt, delegator_info[2]))
     while staked_lpt < LPT_THRESHOLD:
         print("Waiting for staked LPT to reach threshold {0}. Currently has a stake of {1:.2f} LPT. Retrying in {2}...".format(LPT_THRESHOLD, staked_lpt, WAIT_TIME_BALANCE_CHECK))
         time.sleep(WAIT_TIME_BALANCE_CHECK)
         delegator_info = bonding_contract.functions.getDelegator(delegator).call()
-        staked_lpt = web3.Web3.fromWei(delegator_info[0], 'ether')
+        staked_lpt = web3.Web3.fromWei(pending_lptu, 'ether')
     print("Delegator has a stake of {0:.2f} LPT which exceeds the minimum threshold of {1:.2f}, continuing...".format(staked_lpt, LPT_THRESHOLD))
 
 """
@@ -65,7 +67,7 @@ def waitForLock(rounds_contract):
     round_lock = rounds_contract.functions.currentRoundLocked().call()
     while not round_lock:
         print("Waiting for round to become locked. Retrying in {0} seconds...".format(WAIT_TIME_ROUND_LOCK))
-        time.sleep(WAIT_TIME_ROUND_LOCK);
+        time.sleep(WAIT_TIME_ROUND_LOCK)
         round_lock = rounds_contract.functions.currentRoundLocked().call()
     print("Round is locked, continuing...")
 
@@ -78,7 +80,9 @@ def waitForLock(rounds_contract):
 def doTransferBond(bonding_contract, parsed_delegator_wallet, parsed_destination_wallet):
     delegator_info = bonding_contract.functions.getDelegator(parsed_delegator_wallet).call()
     staked_lpt = web3.Web3.fromWei(delegator_info[0], 'ether')
-    transfer_amount = web3.Web3.toWei(staked_lpt - 1, 'ether')
+    pending_lptu = bonding_contract.functions.pendingStake(parsed_delegator_wallet, 99999).call()
+    pending_lpt = web3.Web3.fromWei(pending_lptu, 'ether')
+    transfer_amount = web3.Web3.toWei(pending_lpt - 1, 'ether')
     print("Should transfer {0} LPTU".format(transfer_amount))
     # Build transaction info
     tx = bonding_contract.functions.transferBond(parsed_destination_wallet, transfer_amount,
@@ -123,4 +127,3 @@ if __name__ == "__main__":
         waitForStake(bonding_contract, parsed_delegator_wallet)
         waitForLock(rounds_contract)
         doTransferBond(bonding_contract, parsed_delegator_wallet, parsed_destination_wallet)
-
